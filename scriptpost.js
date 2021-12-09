7eecaa2db5c71ab4a6357a88dd11388893160466
Carlos Gozalbes <carlosfgmendoza@gmail.com>
2021-12-09 14:10:47 +0100

    alert post

--- scriptpost.js
+++ scriptpost.js
@@ -10,7 +10,7 @@
 const postProduct = async () => {
     
     
-    
+    try {
     const response = await fetch("https://striveschool-api.herokuapp.com/api/product/", {
     method: 'POST',   
     body: JSON.stringify({
@@ -24,5 +24,14 @@ const postProduct = async () => {
     headers:  {
         "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWIwYWU5ZjRjZmY1ZjAwMTU5MGJkYjAiLCJpYXQiOjE2Mzg5Njg5OTIsImV4cCI6MTY0MDE3ODU5Mn0.GCy-MTuNh6IsfcfFG90Jt2Hw_g4dGbNBBfCDM3D9MQ0",
         "Content-Type": "application/json"
-    }    
-})}
+    } 
+    })
+    if (response.ok) {
+        const serverResp = await response.json()
+        alert("Appointment created successfully with an id of: " + serverResp._id)
+    }   
+
+    } catch (err) {
+        alert(err)
+    }   
+}
